0582d312d702b8463ce3d74239769ce7d64042bd
Lisa Malenfant <lisa.malenfant@uci.edu>
2022-03-11 09:33:07 -0800

    Fixed the issue with the plots with independent axes

--- src/app/axis/axis.model.ts
+++ src/app/axis/axis.model.ts
@@ -2,7 +2,7 @@ import { AxisRange } from "./axis-range.model";
 
 export class Axis {
     public axis: string = "";
-    public axisRange: AxisRange = {start: 0.5, stop: 9.5, count: 10};
+    public axisRange: AxisRange | null = null;
     public axisValue: number = 0;
   }
   
